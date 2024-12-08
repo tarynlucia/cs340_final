0c311eb02fefb2fbe85139a450389eae4b27b224
thientu745 <thientu745@gmail.com>
2024-12-08 15:16:56 -0800

    configures the members table and displays it on the index page

--- index.php
+++ index.php
@@ -43,15 +43,15 @@
             <div class="row">
                 <div class="col-md-12">
 		    <div class="page-header clearfix">
-		     <h2> Sample Project CS 340 </h2> 
+		     <h2> SASE Website Project CS 340 </h2> 
                        <p> Project should include CRUD operations. In this website you can:
 				<ol> 	<li> CREATE new employess and  dependents </li>
 					<li> RETRIEVE all dependents and prjects for an employee</li>
                                         <li> UPDATE employeee and dependent records</li>
 					<li> DELETE employee and dependent records </li>
 				</ol>
-		       <h2 class="pull-left">Employee Details</h2>
-                        <a href="createEmployee.php" class="btn btn-success pull-right">Add New Employee</a>
+		       <h2 class="pull-left">Member Details</h2>
+                        <!-- <a href="createEmployee.php" class="btn btn-success pull-right">Add New Employee</a> -->
                     </div>
                     <?php
                     // Include config file
@@ -64,42 +64,35 @@
 						$sql = "SELECT Ssn,Fname,Lname,Salary, Address, Bdate, PayLevel(Ssn) as Level, Super_ssn, Dno
 							FROM EMPLOYEE";
 					*/
-                    $sql = "SELECT Ssn,Fname,Lname,Salary, Address, Bdate, PayLevel(Ssn) as Level, Super_ssn, Dno
-							FROM EMPLOYEE";
+                    $sql = "SELECT *
+							FROM Members";
                     if($result = mysqli_query($link, $sql)){
                         if(mysqli_num_rows($result) > 0){
                             echo "<table class='table table-bordered table-striped'>";
                                 echo "<thead>";
                                     echo "<tr>";
-                                        echo "<th width=8%>SSN</th>";
+                                        echo "<th width=10%>OSU ID</th>";
                                         echo "<th width=10%>First Name</th>";
                                         echo "<th width=10%>Last Name</th>";
-                                        echo "<th width=15%>Address </th>";
-										echo "<th width=10%>Birthdate </th>";
-										echo "<th width = 5%>Salary</th>";
-                                        echo "<th width=10%> Level  </th>";
-                                        echo "<th width =8%>Super SSN  </th>";
-										echo "<th width =5%>Dno  </th>";
-                                        echo "<th width=10%>Action</th>";
+                                        echo "<th width=10%>Year</th>";
+										echo "<th width=10%>Major</th>";
+										echo "<th width =10%>Group Number</th>";
                                     echo "</tr>";
                                 echo "</thead>";
                                 echo "<tbody>";
                                 while($row = mysqli_fetch_array($result)){
                                     echo "<tr>";
-                                        echo "<td>" . $row['Ssn'] . "</td>";
+                                        echo "<td>" . $row['OSU_ID'] . "</td>";
                                         echo "<td>" . $row['Fname'] . "</td>";
                                         echo "<td>" . $row['Lname'] . "</td>";
-										echo "<td>" . $row['Address'] . "</td>";									
-										echo "<td>" . $row['Bdate'] . "</td>";
-                                        echo "<td>" . $row['Salary'] . "</td>";										
-                                        echo "<td>" . $row['Level'] . "</td>";
-										echo "<td>" . $row['Super_ssn'] . "</td>";
-                                        echo "<td>" . $row['Dno'] . "</td>";
+										echo "<td>" . $row['Year'] . "</td>";									
+										echo "<td>" . $row['Major'] . "</td>";
+                                        echo "<td>" . $row['Group_number'] . "</td>";
                                         echo "<td>";
-                                            echo "<a href='viewProjects.php?Ssn=". $row['Ssn']."&Lname=".$row['Lname']."' title='View Projects' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
-                                            echo "<a href='updateEmployee.php?Ssn=". $row['Ssn'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
-                                            echo "<a href='deleteEmployee.php?Ssn=". $row['Ssn'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
-											echo "<a href='viewDependents.php?Ssn=". $row['Ssn']."&Lname=".$row['Lname']."' title='View Dependents' data-toggle='tooltip'><span class='glyphicon glyphicon-user'></span></a>";
+                                            // echo "<a href='viewProjects.php?Ssn=". $row['Ssn']."&Lname=".$row['Lname']."' title='View Projects' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
+                                            // echo "<a href='updateEmployee.php?Ssn=". $row['Ssn'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
+                                            // echo "<a href='deleteEmployee.php?Ssn=". $row['Ssn'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+											// echo "<a href='viewDependents.php?Ssn=". $row['Ssn']."&Lname=".$row['Lname']."' title='View Dependents' data-toggle='tooltip'><span class='glyphicon glyphicon-user'></span></a>";
                                         echo "</td>";
                                     echo "</tr>";
                                 }
@@ -112,42 +105,6 @@
                         }
                     } else{
                         echo "ERROR: Could not able to execute $sql. <br>" . mysqli_error($link);
-                    }
-					echo "<br> <h2> Department Stats </h2> <br>";
-					
-                    // Select Department Stats
-					// You will need to Create a DEPT_STATS table
-					
-                    $sql2 = "SELECT * FROM DEPT_STATS";
-                    if($result2 = mysqli_query($link, $sql2)){
-                        if(mysqli_num_rows($result2) > 0){
-                            echo "<div class='col-md-4'>";
-							echo "<table width=30% class='table table-bordered table-striped'>";
-                                echo "<thead>";
-                                    echo "<tr>";
-                                        echo "<th width=20%>Dno</th>";
-                                        echo "<th width = 20%>Number of Employees</th>";
-                                        echo "<th width = 40%>Average Salary</th>";
-	
-                                echo "</thead>";
-                                echo "<tbody>";
-                                while($row = mysqli_fetch_array($result2)){
-                                    echo "<tr>";
-                                        echo "<td>" . $row['Dnumber'] . "</td>";
-                                        echo "<td>" . $row['Emp_count'] . "</td>";
-                                        echo "<td>" . $row['Avg_salary'] . "</td>";
-               
-                                    echo "</tr>";
-                                }
-                                echo "</tbody>";                            
-                            echo "</table>";
-                            // Free result set
-                            mysqli_free_result($result2);
-                        } else{
-                            echo "<p class='lead'><em>No records were found for Dept Stats.</em></p>";
-                        }
-                    } else{
-                        echo "ERROR: Could not able to execute $sql2. <br>" . mysqli_error($link);
                     }
 					
                     // Close connection
